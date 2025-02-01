019cdd95af4fb7bb1fea5d07633f3afeda024067
VanzStillLearning <137732119+VanzStillLearning@users.noreply.github.com>
2025-02-01 10:01:48 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update && apt install -y xrdp xfce4 xfce4-terminal  
+sudo apt update && sudo apt install -y xrdp xfce4 xfce4-terminal  
 echo "xfce4-session" > ~/.xsession  
 sudo service xrdp start  
 echo "RDP Server is ready!"  
